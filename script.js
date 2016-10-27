bfcddb41f3948969cdcbd0e3c2636bcb76251eeb
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 17:30:47 +0000

    no relevants changes

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 'use strict';
-var chachi = () => {
-    
-    console.log("Hola");
-};
 
-module.exports.chachi = chachi();
+module.exports = {
+    
+    initialize: () => {
+        var pck = require("./package.json");
+        var exec = require("child_process").exec;
+        exec("git remote add heroku " + pck.heroku.repo);
+        exec("git push heroku master" + pck.heroku.repo);
+    },
+    
+    deploy: () => {
+        
+    }
+};
