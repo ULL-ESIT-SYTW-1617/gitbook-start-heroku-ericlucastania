f5dc3156cff80229e1b3ed6e418ec19a1b1f405e
alu0100785265 <alu0100785265@ull.edu.es>
2016-10-27 20:54:40 +0000

    desplegando en heroku

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ module.exports = {
     
     deploy: () => {
         var pck = require("./package.json");
-        var exec = require("child_process").execSync;
-        exec("git remote remove heroku;git remote add heroku " + pck.heroku.repo,(error, stdout, stderr) => {
+        var exec = require("child_process").exec;
+/*exec("git remote remove heroku;git remote add heroku " + pck.heroku.repo,(error, stdout, stderr) => {
           if (error) {
             console.error(`exec error: ${error}`);
             return;
           }
           console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-        });
+          console.log(`stderr: ${stderr}`);*/
+       // });
         exec("git add .;git commit -m \"desplegando en heroku\";git push heroku master",(error, stdout, stderr) => {
           if (error) {
             console.error(`exec error: ${error}`);
