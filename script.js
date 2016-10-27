11409eab23bf2c67409d84c7b5e636fcb98d680a
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-27 17:40:28 +0000

    cambios en el script

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 module.exports = {
     
     initialize: () => {
+        
+        console.log("HOLAAAA");
+    },
+    
+    deploy: () => {
         var pck = require("./package.json");
         var exec = require("child_process").exec;
         exec("git remote add heroku " + pck.heroku.repo);
         exec("git push heroku master" + pck.heroku.repo);
-    },
-    
-    deploy: () => {
-        
     }
 };
