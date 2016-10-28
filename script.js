00b787e60e74627920ba3d35f04f9d7a330202ba
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-28 09:45:56 +0000

    quitando lineas del script no relevantes

--- script.js
+++ script.js
@@ -31,24 +31,9 @@ module.exports = {
     
     deploy: () => {
          var pck = require("./package.json");
-         var exec = require("child_process").exec;
-         exec(process.cwd() + '/' + "echo web: node  app.js > Procfile ");
-         exec("git remote remove heroku;git remote add heroku " + pck.heroku.repo,(error, stdout, stderr) => {
-                if (error) {
-                  console.error(`exec error: ${error}`);
-                  return;
-                }
-                console.log(`stdout: ${stdout}`);
-                console.log(`stderr: ${stderr}`);
-              });
-         exec("git add .;git commit -m \"desplegando en heroku\";git push heroku master",(error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-        });
+         require('shelljs/global');
+         exec("git remote add heroku ");
+         exec("git add .;git commit -m \"desplegando en heroku\";git push heroku master")
     }
     
 };
