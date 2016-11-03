ef486a368aaa29cb217380ff52457a1988a99388
tania77 <ainatrg@gmail.com>
2016-11-03 12:13:47 +0000

    arreglando issue

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ module.exports = {
         
         
         var ruta = path.join(__dirname, 'template', 'gulpfile.js');
-        var ruta2 = path.join(__dirname, 'template', 'app.js');
-        var ruta3 = path.join(__dirname, 'template', 'Procfile');
+        var ruta2 = path.join(__dirname, 'template');
+        //var ruta3 = path.join(__dirname, 'template', 'Procfile');
           
       
       
@@ -28,9 +28,16 @@ module.exports = {
         });
         
         
-        
-        exec("cp " + ruta2 + " " + direct);
-        exec("cp " + ruta3 + " " + direct);
+        fs.readdir(ruta2, (err, files) => {
+            var pos =files.indexOf("gulpfile.js");
+            files.splice(pos,1);
+            files.forEach((archivo) => {
+                cp (archivo, direct); 
+            });
+            
+        });
+        //exec("cp " + ruta2 + " " + direct);
+        //exec("cp " + ruta3 + " " + direct);
         
     },
     
@@ -50,7 +57,6 @@ module.exports = {
         exec("git add .;git commit -m \"desplegando en heroku\";git push heroku master",(error, stdout, stderr) => {
 
          var pck = require("./package.json");
-<<<<<<< HEAD
          var exec = require("child_process").exec;
          exec(process.cwd() + '/' + "echo web: node  app.js > Procfile ");
          exec("git remote remove heroku;git remote add heroku " + pck.heroku.repo,(error, stdout, stderr) => {
@@ -70,11 +76,9 @@ module.exports = {
           console.log(`stdout: ${stdout}`);
           console.log(`stderr: ${stderr}`);
         });
-=======
          require('shelljs/global');
          exec("git remote add heroku " + pck.heroku.repo);
          exec("git add .;git commit -m \"desplegando en heroku\";git push heroku master");
->>>>>>> 50c8308b222edb69dafb977e9fdad889c85e3c35
     }
     
 };
