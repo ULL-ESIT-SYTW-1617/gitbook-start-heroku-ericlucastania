8232aa518f0ff9069375764d730a6bddce0c1075
alu0100785265 <alu0100785265@ull.edu.es>
2016-11-10 11:47:23 +0000

    cambios

--- template/app.js
+++ template/app.js
@@ -3,12 +3,39 @@
 
 var express = require('express');
 var app = express();
-var  path = require('path');
 var  expressLayouts = require('express-ejs-layouts');
-var proces = require('child_process');
+
+var passport = require('passport');
+var Strategy = require('passport-github').Strategy;
 
 
 
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+passport.use(new Strategy({
+    clientID: '217bf6cd072238e4f2d1',
+    clientSecret: '3aac244b495a7fda4e113c46d8db90eeec137201',
+    callbackURL: 'http://127.0.0.1:8080:/login/github/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's GitHub profile is supplied as the user
+    // record.  In a production-quality application, the GitHub profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+  
+  
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.set('port', (process.env.PORT || 8080));
@@ -16,26 +43,29 @@ app.set('port', (process.env.PORT || 8080));
 
 app.use(expressLayouts);
 
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('gh-pages'));
 
 
-app.get('/', function(request, response){
-  response.send('index');  
+//routes
+
+
+app.get('/login',function(req, res){
+    res.send('login');
+});
+
+app.get('/login/github',passport.authenticate('github'));
+
+app.get('/login/github/return', passport.authenticate('github', { failureRedirect: '/login' }),function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/profile',require('connect-ensure-login').ensureLoggedIn(),function(req, res){
+    res.render('profile', { user: req.user });
 });
 
 
-app.post('/syn', function(request, response){
-    
-    proces.exec("git pull origin master;node app.js", (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      
-      response.send(console.log(stdout));
-    });
-    
+app.get('/', function(request, response){
+  response.send('index',{ user: request.user });  
 });
 
 
@@ -45,3 +75,9 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
+
+
+
+
+
+
