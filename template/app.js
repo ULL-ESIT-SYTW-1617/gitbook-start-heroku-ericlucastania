20a4da194d2a0dda5a29fcc4642b9a75c6dbb013
José Lucas <alu0100785265@ull.edu.es>
2016-11-11 11:17:00 +0000

    Update app.js

--- template/app.js
+++ template/app.js
@@ -3,12 +3,38 @@
 
 var express = require('express');
 var app = express();
-var  expressLayouts = require('express-ejs-layouts');
-
+var path = require('path');
 var passport = require('passport');
 var Strategy = require('passport-github').Strategy;
 
 
+passport.use(new Strategy({
+    clientID: '217bf6cd072238e4f2d1',
+    clientSecret: '3aac244b495a7fda4e113c46d8db90eeec137201',
+    callbackURL: 'http://localhost:8080/login/github/return'
+},function(accessToken, refreshToken, profile, cb) {
+    var github = require('octonode');
+    var client = github.client();
+    var ghorg          = client.org('ULL-ESIT-SYTW-1617');
+    ghorg.members(function(err,stdout){
+      console.log(stdout);
+      if(err) console.log(err);
+        for (var i in stdout){
+          if(stdout[i].login == profile.name){
+            console.log(stdout[i].login);
+          }
+        }      
+    });
+    return cb(null, profile);
+  }));
+  
+
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+  
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -19,38 +45,49 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 
-passport.use(new Strategy({
-    clientID: '217bf6cd072238e4f2d1',
-    clientSecret: '3aac244b495a7fda4e113c46d8db90eeec137201',
-    callbackURL: 'http://127.0.0.1:8080:/login/github/return'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's GitHub profile is supplied as the user
-    // record.  In a production-quality application, the GitHub profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
-  
-  
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'gh-pages')));
 
 app.set('port', (process.env.PORT || 8080));
 
+app.set('view engine', 'ejs');
 
-app.use(expressLayouts);
 
-app.use(express.static('gh-pages'));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 
 
+app.get('/', function(req, res){
+    
+  res.render('home',{user: req.user}); 
+    
+});
+
+app.get('/book',function(req, res) {
+  
+  res.sendfile('gh-pages/juanito.html');
+  
+});
+
+app.get('/home',function(req, res) {
+   res.render('home',{user: req.user}); 
+});
+
 app.get('/login',function(req, res){
-    res.send('login');
+
+  app.get('/profile',function(req, res) {
+     res.render('home'); 
+  });
+
+    res.render('login');
 });
 
 app.get('/login/github',passport.authenticate('github'));
@@ -59,14 +96,10 @@ app.get('/login/github/return', passport.authenticate('github', { failureRedirec
     res.redirect('/');
 });
 
-app.get('/profile',require('connect-ensure-login').ensureLoggedIn(),function(req, res){
-    res.render('profile', { user: req.user });
+app.get('/profile',require('connect-ensure-login').ensureLoggedIn(),function(req, res,next){
+  res.render('profile',{user: req.user});
 });
 
-
-app.get('/', function(request, response){
-  response.send('index',{ user: request.user });  
-});
 
 
 
