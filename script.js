58789fb45fd2824f4a58168ead759d7daf62bbe6
tania77 <ainatrg@gmail.com>
2016-11-18 14:19:12 +0000

    corrigiendo errores

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ module.exports = {
       if ((auth == 's') || (auth == 'S') || (auth == '')) {
         var posapp = files.indexOf("app.js");
         files.splice(posapp,1);
-        fs.writeFileSync(dir + 'Procfile', 'web: node appAuth.js');
+        fs.writeFileSync(directorioPlugin2 + 'Procfile', 'web: node appAuth.js');
       }
       else if ((auth == 'n') || (auth == 'N')) {
         var posappAuth = files.indexOf("appAuth.js");
